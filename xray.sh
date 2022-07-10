b61ca512f73b0365bc58c2bfee32c2c3129cec1c
crosspacific <48462348+crosspacific@users.noreply.github.com>
2022-07-10 17:27:31 +0800

    Create xray.sh

---
+++ xray.sh
@@ -0,0 +1,48 @@
+
+#!/bin/sh
+
+# config
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    },
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
